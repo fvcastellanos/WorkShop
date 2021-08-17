ce2946dea3440a9ae630224c04ab4eebcab28643
Francisco Castellanos <fvcastellanos@gmail.com>
2021-08-16 20:04:30 -0600

    add Order entity

--- WorkShop/Model/Order.cs
+++ WorkShop/Model/Order.cs
@@ -10,5 +10,17 @@ namespace WorkShop.Model
         [Key]
         [Column("id", TypeName = "varchar(50)")]
         public Guid Id { get; set; }
+
+        [ForeignKey("customer_id")]
+        public Customer Customer { get; set; }
+
+        [Column("plate_number")]
+        public string PlateNumber { get; set; }
+
+        [Column("created", TypeName = "timestamp")]
+        public DateTime Created { get; set; }
+
+        [Column("image_url", TypeName = "varchar(500)")]
+        public string ImageUrl { get; set; }
     }
 }
